fbe0838f5c485c224183a0a7b1ad53798697c978
coderanik <anikdas210605@gmail.com>
2025-01-03 00:07:08 +0530

    made changes

--- api/repositories.js
+++ api/repositories.js
@@ -14,24 +14,29 @@ const fetchRepositories = async (url, headers, retries = 3) => {
   }
 };
 
-module.exports = async (req, res) => {
-  const username = 'coderanik'; 
-  const apiUrl = `https://api.github.com/users/${username}/repos?sort=created&direction=desc`;
 
-  const GITHUB_TOKEN = process.env.GITHUB_TOKEN || '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'; 
+
+module.exports = async (req, res) => {
+  const username = 'coderanik';
+  const apiUrl = `https://api.github.com/users/${username}/repos?sort=created&direction=desc`;
+  const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+  if (!GITHUB_TOKEN) {
+    console.error('GitHub token is missing');
+    return res.status(500).json({ error: 'GitHub token is missing in the environment' });
+  }
 
   try {
     const response = await axios.get(apiUrl, {
       headers: {
-        Authorization: `Bearer ${GITHUB_TOKEN}` 
+        Authorization: `Bearer ${GITHUB_TOKEN}`
       }
     });
 
-    // Return only the top 6 repositories
     const recentRepos = response.data.slice(0, 6);
     res.status(200).json(recentRepos);
   } catch (error) {
-    console.error('Error fetching repositories:', error.message);
+    console.error('Error fetching repositories:', error.response?.data || error.message);
     res.status(error.response?.status || 500).json({ error: 'Failed to fetch repositories' });
   }
 };
